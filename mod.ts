453919eb3e3104bd7e523c2b258c8d6e9583d64a
impactvelocity <monetizedesign@gmail.com>
2023-03-10 12:40:02 -0800

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -2,5 +2,5 @@ import { bold } from "./deps.ts";
 
 /** Returns `Hello World` in bold */
 export function getHelloWorld(): string {
-  return bold("Hello World");
+  return bold("Hello World - deno module!");
 }
