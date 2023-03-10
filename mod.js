ae61bad91b1c6f099bcb1dc5632a732312542e97
impactvelocity <monetizedesign@gmail.com>
2023-03-10 12:49:28 -0800

    process

--- mod.js
+++ mod.js
@@ -4,3 +4,25 @@
 export function getHelloWorld() {
 	return "This is from a module! Deno land!!"
 }
+
+export async function process(req) {
+	const body = await req.json()
+
+	return {
+		page: {
+			...body.page,
+			updateText: (id, value) => {
+				console.log(id, value)
+			},
+		},
+		user: {
+			...body.user,
+			updateName: (value) => {
+				body.user.name = value
+			},
+		},
+		location: {
+			...body.location,
+		},
+	}
+}
